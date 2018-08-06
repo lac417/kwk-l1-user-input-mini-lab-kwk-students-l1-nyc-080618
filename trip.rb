b3070e6256f5e0d16d90a8815e2f0d25a17474cd
lac417 <lac417@gmail.com>
2018-08-06 19:32:28 +0000

    automatically backed up by learn

--- trip.rb
+++ trip.rb
@@ -10,11 +10,11 @@ stay = gets.chomp.capitalize  #don't forget to Capitilize the input
 # Do the same thing with `eats` and one last time with `nights`, capturing their text input and storing them to the respective local variable
 
 puts "Where would you like to eat?"
-eats = gets.chomp.capitalize
+eat = gets.chomp
 puts "How many nights would you like to stay?"
-nights = gets.chop.capitalize
+nights = gets.chop
 
 
 # Lastly, puts it all back by interpolating these values in a string.
 
-puts "On my trip, I will stay at #{stay} for #{nights} and eat at #{eats}."
+puts "On my trip, I will stay at #{stay} for #{nights} and eat at #{eat}."
