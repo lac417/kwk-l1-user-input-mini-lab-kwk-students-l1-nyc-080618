f6a7b6a2eeffce2888ddd1d04d191da898d648bd
lac417 <lac417@gmail.com>
2018-08-06 19:14:27 +0000

    automatically backed up by learn

--- userinput.rb
+++ userinput.rb
@@ -1,6 +1,13 @@
 puts "What is your name?"
 name = gets.chomp
 
-puts "What's your favorite channel?"
-channel = gets.chomp
-puts "My name is #{name} and you're watching #{channel}!"
+puts "How old are you?"
+age = gets.chomp 
+
+puts "What school do you go to?"
+school = gets.chomp
+
+puts "What's your favorite show?"
+show = gets. chomp
+
+puts "My name is #{name} and I am #{age}. I go to #{school} and you're watching #{show}!"
