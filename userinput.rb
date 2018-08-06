e2e285628fa40b7118c28cc7c0068cd85c51a7ff
lac417 <lac417@gmail.com>
2018-08-06 19:16:27 +0000

    automatically backed up by learn

--- userinput.rb
+++ userinput.rb
@@ -4,10 +4,10 @@ name = gets.chomp
 puts "How old are you?"
 age = gets.chomp 
 
-puts "What school do you go to?"
-school = gets.chomp
+puts "What's your favorite food?"
+food = gets.chomp
 
 puts "What's your favorite show?"
 show = gets. chomp
 
-puts "My name is #{name} and I am #{age}. I go to #{school} and you're watching #{show}!"
+puts "My name is #{name} and I am #{age}. I love #{food} and #{show}!"
